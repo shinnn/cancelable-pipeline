b5a7f9dad7aa9ca33bae4cfd52a19e792edb24ba
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-05-14 09:37:59 +0900

    update dependencies and devDependencies
    Highlights:
    
    * update ESLint from v3 to v4
    * update @shinnn/eslint-config and follow its rules, for exmaple tab indent
    * update pump from v1 to v3
    * switch from istanbul to nyc

--- index.js
+++ index.js
@@ -5,33 +5,33 @@ const pump = require('pump');
 const cancel = new Error('Canceled.');
 
 module.exports = function cancelablePump() {
-  const nonCallbackArgs = Array.prototype.slice.call(arguments); // eslint-disable-line prefer-rest-params
-  const callback = typeof nonCallbackArgs[nonCallbackArgs.length - 1] === 'function' ?
-                 nonCallbackArgs.pop() :
-                 null;
+	const nonCallbackArgs = Array.prototype.slice.call(arguments); // eslint-disable-line prefer-rest-params
+	const callback = typeof nonCallbackArgs[nonCallbackArgs.length - 1] === 'function' ?
+		nonCallbackArgs.pop() :
+		null;
 
-  const streams = Array.isArray(nonCallbackArgs[0]) ? nonCallbackArgs[0] : nonCallbackArgs;
-  const streamCount = streams.length;
+	const streams = Array.isArray(nonCallbackArgs[0]) ? nonCallbackArgs[0] : nonCallbackArgs;
+	const streamCount = streams.length;
 
-  if (streamCount < 2) {
-    throw new RangeError('cancelable-pump requires more than 2 streams, but got ' + streamCount + '.');
-  }
+	if (streamCount < 2) {
+		throw new RangeError(`cancelable-pump requires more than 2 streams, but got ${streamCount}.`);
+	}
 
-  pump(streams, err => {
-    if (!callback) {
-      return;
-    }
+	pump(streams, err => {
+		if (!callback) {
+			return;
+		}
 
-    if (err && err !== cancel) {
-      callback(err);
-      return;
-    }
+		if (err && err !== cancel) {
+			callback(err);
+			return;
+		}
 
-    callback();
-  });
+		callback();
+	});
 
-  return function cancelStreams() {
-    streams[streamCount - 1].emit('error', cancel);
-  };
+	return function cancelStreams() {
+		streams[streamCount - 1].emit('error', cancel);
+	};
 };
 
--- test.js
+++ test.js
@@ -6,51 +6,51 @@ const cancelablePump = require('.');
 const test = require('tape');
 
 test('cancelablePump()', t => {
-  t.plan(3);
+	t.plan(3);
 
-  t.doesNotThrow(
-    () => cancelablePump([new PassThrough(), new PassThrough()])(),
-    'should return a function.'
-  );
+	t.doesNotThrow(
+		() => cancelablePump([new PassThrough(), new PassThrough()])(),
+		'should return a function.'
+	);
 
-  const cancel = cancelablePump(new PassThrough(), new PassThrough(), err => {
-    t.strictEqual(
-      err,
-      undefined,
-      'should cancel all streams when the cancellation function is called.'
-    );
-  });
+	const cancel = cancelablePump(new PassThrough(), new PassThrough(), err => {
+		t.equal(
+			err,
+			undefined,
+			'should cancel all streams when the cancellation function is called.'
+		);
+	});
 
-  cancel();
+	cancel();
 
-  const dest1 = new PassThrough();
-  const error = new Error();
+	const dest1 = new PassThrough();
+	const error = new Error();
 
-  cancelablePump(new PassThrough(), dest1, err => {
-    t.strictEqual(err, error, 'should pass normal errors as it is.');
-  });
+	cancelablePump(new PassThrough(), dest1, err => {
+		t.equal(err, error, 'should pass normal errors as it is.');
+	});
 
-  dest1.emit('error', error);
+	dest1.emit('error', error);
 });
 
 test('Argument validation', t => {
-  t.throws(
-    () => cancelablePump(),
-    /^RangeError: cancelable-pump requires more than 2 streams, but got 0\.$/,
-    'should throw an error when it takes no arguments.'
-  );
+	t.throws(
+		() => cancelablePump(),
+		/^RangeError: cancelable-pump requires more than 2 streams, but got 0\.$/,
+		'should throw an error when it takes no arguments.'
+	);
 
-  t.throws(
-    () => cancelablePump(new PassThrough()),
-    /^RangeError: cancelable-pump requires more than 2 streams, but got 1\.$/,
-    'should throw an error when it takes only 1 argument.'
-  );
+	t.throws(
+		() => cancelablePump(new PassThrough()),
+		/^RangeError: cancelable-pump requires more than 2 streams, but got 1\.$/,
+		'should throw an error when it takes only 1 argument.'
+	);
 
-  t.throws(
-    () => cancelablePump(new PassThrough(), t.fail),
-    /^RangeError: cancelable-pump requires more than 2 streams, but got 1\.$/,
-    'should throw an error when the arguments include less than 2 streams.'
-  );
+	t.throws(
+		() => cancelablePump(new PassThrough(), t.fail),
+		/^RangeError: cancelable-pump requires more than 2 streams, but got 1\.$/,
+		'should throw an error when the arguments include less than 2 streams.'
+	);
 
-  t.end();
+	t.end();
 });
